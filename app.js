595db55d53fe66f3a2bb7c2f457e9195e7b0644b
pius Polocha <piuspoloch1231@gmail.com>
2023-12-19 10:30:18 +0100

    ready to deploy

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose  = require('mongoose');
+const cookieParser = require('cookie-parser');
+const methodOverride = require('method-override');
+// const indexRoute = require('./server/Route/indexRoute');
+// const userRoute = require('./server/Route/userRoute');
+// const adminRoute = require('./server/Route/adminRoute');
+const { requireAuth, checkUser } = require('./server/authMiddleware/authMiddleware');
+const connectDB = require('./server/db');
+
+
+const app = express();
+const port = 5000 || process.env.PORT;
+
+//middlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+
+// set view engine
+app.set('view engine', 'ejs');
+
+//db connection
+// const dbUrl = 'mongodb+srv://pius1:pius123@webdevelopment.xav1dsx.mongodb.net/fxlite';
+// mongoose.connect(dbUrl)
+// .then(() =>{
+//     app.listen(port, () =>{
+//         console.log(`app is listening ${port}`)
+//     })
+// })
+// .catch((error) =>{
+//     console.log(error);
+// });
+
+//db connection
+connectDB();
+app.listen(port, () =>{
+    console.log(`app is listening ${port}`)
+})
+
+// app.get('*', checkUser);
+// app.use('/', requireAuth, require );
+// app.use('/',requireAuth, userRoute);
+// app.use('/', requireAuth, adminRoute);
+
+app.get('*', checkUser);
+app.use('/', require('./server/Route/indexRoute'));
+app.use('/',requireAuth, require('./server/Route/userRoute'));
+app.use('/', require('./server/Route/adminRoute'));
+
+
+
---
+++ server/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false)
+
+const connectDB = async() =>{
+    try {
+        const conn = await mongoose.connect('mongodb+srv://pius1:pius123@webdevelopment.xav1dsx.mongodb.net/fxlite')
+        console.log(`Database connection ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+module.exports = connectDB;
