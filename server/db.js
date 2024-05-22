718dbd5f5cd74ff109d38974a137e340deeb1bab
Pius Polocha <117569002+Piuscoding@users.noreply.github.com>
2024-05-22 16:02:20 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ mongoose.set('strictQuery', false)
 
 const connectDB = async() =>{
     try {
-        const conn = await mongoose.connect('mongodb+srv://pius1:pius123@webdevelopment.xav1dsx.mongodb.net/fxlite')
+        const conn = await mongoose.connect('mongodb+srv://pius1:pius123@webdevelopment.xav1dsx.mongodb.net/masitrade')
         console.log(`Database connection ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
